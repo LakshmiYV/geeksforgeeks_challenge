2a885e9da6b5b39480171159c9a2c8325c07d5c2
LakshmiYV <161949763+LakshmiYV@users.noreply.github.com>
2024-08-03 11:21:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Asteroid Collision/asteroid-collision.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+
+            int N;
+            N = sc.nextInt();
+
+            int[] asteroids = new int[N];
+            for (int i = 0; i < N; i++) asteroids[i] = sc.nextInt();
+
+            Solution obj = new Solution();
+            int[] res = obj.asteroidCollision(N, asteroids);
+
+            for (int e : res) System.out.print(e + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static int[] asteroidCollision(int N, int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int asteroid : asteroids) {
+            boolean exploded = false;
+            
+            while (!stack.isEmpty() && asteroid < 0 && stack.peek() > 0) {
+                if (stack.peek() < -asteroid) {
+                    stack.pop();
+                    continue;
+                } else if (stack.peek() == -asteroid) {
+                    stack.pop();
+                }
+                exploded = true;
+                break;
+            }
+            
+            if (!exploded) {
+                stack.push(asteroid);
+            }
+        }
+        
+        int[] result = new int[stack.size()];
+        for (int i = result.length - 1; i >= 0; i--) {
+            result[i] = stack.pop();
+        }
+        
+        return result;
+    }
+}
+
