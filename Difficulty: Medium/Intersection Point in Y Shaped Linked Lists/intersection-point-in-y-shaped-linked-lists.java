c3d02016361b9edef9411e98fbb59a504df7913d
LakshmiYV <161949763+LakshmiYV@users.noreply.github.com>
2024-11-13 19:12:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Intersection Point in Y Shaped Linked Lists/intersection-point-in-y-shaped-linked-lists.java
@@ -0,0 +1,151 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class LinkedList_Intersection {
+    Node head = null;
+    Node tail = null;
+
+    public void addToTheLast(Node node) {
+
+        if (head == null) {
+            head = node;
+            tail = head;
+        } else {
+            // Node temp = head;
+            // while (temp.next != null)
+            // temp = temp.next;
+
+            // temp.next = node;
+            tail.next = node;
+            tail = node;
+        }
+    }
+
+    /* Function to print linked list */
+    void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    /* Driver program to test above functions */
+    public static void main(String args[]) throws IOException {
+
+        /* Constructed Linked List is 1->2->3->4->5->6->
+           7->8->8->9->null */
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t > 0) {
+
+            LinkedList_Intersection llist1 = new LinkedList_Intersection();
+            LinkedList_Intersection llist2 = new LinkedList_Intersection();
+            LinkedList_Intersection llist3 = new LinkedList_Intersection();
+
+            String str[] = read.readLine().trim().split(" ");
+            int n1 = str.length;
+            Node head1 = new Node(Integer.parseInt(str[0]));
+
+            Node tail1 = head1;
+
+            for (int i = 1; i < n1; i++) {
+                int a = Integer.parseInt(str[i]);
+                tail1.next = (new Node(a));
+                tail1 = tail1.next;
+            }
+
+            str = read.readLine().trim().split(" ");
+            Node head2 = new Node(Integer.parseInt(str[0]));
+            Node tail2 = head2;
+            int n2 = str.length;
+            for (int i = 1; i < n2; i++) {
+                int b = Integer.parseInt(str[i]);
+                tail2.next = (new Node(b));
+                tail2 = tail2.next;
+            }
+
+            str = read.readLine().trim().split(" ");
+            int n3 = str.length;
+            if (n3 > 0) {
+
+                Node head3 = new Node(Integer.parseInt(str[0]));
+                tail1.next = head3;
+                tail2.next = head3;
+                Node tail3 = head3;
+                for (int i = 1; i < n3; i++) {
+                    int c = Integer.parseInt(str[i]);
+                    tail3.next = (new Node(c));
+                    tail3 = tail3.next;
+                }
+            }
+            Intersect obj = new Intersect();
+            System.out.println(obj.intersectPoint(head1, head2));
+            t--;
+        
+System.out.println("~");
+}
+    }
+}
+
+// } Driver Code Ends
+
+
+class Intersect {
+    // Function to find the intersection point in Y-shaped linked lists.
+    int intersectPoint(Node head1, Node head2) {
+        // Calculate the lengths of both linked lists
+        int len1 = getLength(head1);
+        int len2 = getLength(head2);
+
+        // Move the pointer of the longer list forward by the length difference
+        if (len1 > len2) {
+            head1 = moveForwardBy(head1, len1 - len2);
+        } else {
+            head2 = moveForwardBy(head2, len2 - len1);
+        }
+
+        // Traverse both lists together until the intersection point or end
+        while (head1 != null && head2 != null) {
+            if (head1 == head2) {
+                return head1.data;  // Found the intersection point
+            }
+            head1 = head1.next;
+            head2 = head2.next;
+        }
+        
+        return -1;  // No intersection point
+    }
+
+    // Helper function to get the length of a linked list
+    private int getLength(Node head) {
+        int length = 0;
+        while (head != null) {
+            length++;
+            head = head.next;
+        }
+        return length;
+    }
+
+    // Helper function to move a pointer forward by 'steps' nodes
+    private Node moveForwardBy(Node head, int steps) {
+        while (steps > 0 && head != null) {
+            head = head.next;
+            steps--;
+        }
+        return head;
+    }
+}
+
