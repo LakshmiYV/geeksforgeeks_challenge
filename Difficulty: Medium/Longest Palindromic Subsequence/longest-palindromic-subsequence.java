29b8e846ca7be1aa641b5472c42fe6a0f7b297ed
LakshmiYV <161949763+LakshmiYV@users.noreply.github.com>
2025-03-07 11:01:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Palindromic Subsequence/longest-palindromic-subsequence.java
+++ Difficulty: Medium/Longest Palindromic Subsequence/longest-palindromic-subsequence.java
@@ -1,49 +1,48 @@
 //{ Driver Code Starts
-//Initial Template for Java
+// Initial Template for Java
 
 import java.io.*;
 import java.util.*;
 
-class GfG
-{
-    public static void main(String args[])
-        {
-            Scanner sc = new Scanner(System.in);
-            int t = sc.nextInt();
-            while(t-->0)
-                {
-                    String s = sc.next();
-                    Solution obj = new Solution();
-                    System.out.println(obj.longestPalinSubseq(s));
-                }
-                
+class GfG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            String s = sc.next();
+            Solution obj = new Solution();
+            System.out.println(obj.longestPalinSubseq(s));
+
+            System.out.println("~");
         }
+    }
 }
 // } Driver Code Ends
 
 
 class Solution {
-    public int longestPalinSubseq(String S) {
-        int n = S.length();
+    public int longestPalinSubseq(String s) {
+        int n = s.length();
         int[][] dp = new int[n][n];
-        
+
+        // Every single character is a palindrome of length 1
         for (int i = 0; i < n; i++) {
             dp[i][i] = 1;
         }
-        
-        for (int cl = 2; cl <= n; cl++) { // cl is the length of the current substring
-            for (int i = 0; i < n - cl + 1; i++) {
-                int j = i + cl - 1;
-                if (S.charAt(i) == S.charAt(j) && cl == 2) {
-                    dp[i][j] = 2;
-                } else if (S.charAt(i) == S.charAt(j)) {
-                    dp[i][j] = dp[i + 1][j - 1] + 2;
+
+        // Build the table in bottom-up manner
+        for (int len = 2; len <= n; len++) {  // len is the length of substring
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;  // Endpoint of the substring
+                
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = 2 + dp[i + 1][j - 1];  // Extend palindrome
                 } else {
-                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);  // Take max
                 }
             }
         }
-        
-        return dp[0][n - 1];
+
+        return dp[0][n - 1]; // Full string result
     }
 }
