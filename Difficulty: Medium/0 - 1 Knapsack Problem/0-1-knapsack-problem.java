254e69f1c61b827202fe19330cfe6ef3e0c1dd11
LakshmiYV <161949763+LakshmiYV@users.noreply.github.com>
2025-03-13 20:59:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -1,72 +1,50 @@
 //{ Driver Code Starts
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-class gfg {
-    public static void main(String[] args) throws Exception {
+public class GFG {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
+        int testCases = Integer.parseInt(br.readLine());
 
-        while (t-- > 0) {
-            int w = Integer.parseInt(br.readLine());
+        while (testCases-- > 0) {
+            int capacity = Integer.parseInt(br.readLine());
+            String[] valInput = br.readLine().split(" ");
+            String[] wtInput = br.readLine().split(" ");
 
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-            List<Integer> array = new ArrayList<>();
+            int[] val = new int[valInput.length];
+            int[] wt = new int[wtInput.length];
 
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                array.add(Integer.parseInt(token));
+            for (int i = 0; i < valInput.length; i++) {
+                val[i] = Integer.parseInt(valInput[i]);
             }
 
-            int[] val = new int[array.size()];
-            int idx = 0;
-            for (int i : array) val[idx++] = i;
-
-            String lin = br.readLine();
-            String[] toke = lin.split(" ");
-            List<Integer> array1 = new ArrayList<>();
-
-            // Parse the tokens into integers and add to the array
-            for (String token : toke) {
-                array1.add(Integer.parseInt(token));
+            for (int i = 0; i < wtInput.length; i++) {
+                wt[i] = Integer.parseInt(wtInput[i]);
             }
 
-            int[] wt = new int[array1.size()];
-            idx = 0;
-            for (int i : array1) wt[idx++] = i;
-
-            // calling method knapSack() of class Solution
-            System.out.println(new Solution().knapSack(w, wt, val));
+            System.out.println(Solution.knapsack(capacity, val, wt));
+            System.out.println("~");
         }
     }
 }
+
 // } Driver Code Ends
 
 
 class Solution {
-    // Function to return max value that can be put in knapsack of capacity W.
-    static int knapSack(int W, int wt[], int val[]) {
+    static int knapsack(int W, int val[], int wt[]) {
         int n = val.length;
-        // Create a DP array
-        int dp[][] = new int[n+1][W+1];
-        
-        // Iterate over all items
-        for (int i = 1; i <= n; i++) {
-            // Iterate over all capacities from 1 to W
-            for (int w = 1; w <= W; w++) {
-                // If including the item is possible
-                if (wt[i-1] <= w) {
-                    dp[i][w] = Math.max(dp[i-1][w], val[i-1] + dp[i-1][w-wt[i-1]]);
-                } else {
-                    // If the item is not included
-                    dp[i][w] = dp[i-1][w];
-                }
+        int[] dp = new int[W + 1]; // DP array to store max values
+
+        for (int i = 0; i < n; i++) {
+            // Traverse backwards to prevent overwriting
+            for (int j = W; j >= wt[i]; j--) {
+                dp[j] = Math.max(dp[j], val[i] + dp[j - wt[i]]);
             }
         }
         
-        // Return the maximum value that can be put in the knapsack
-        return dp[n][W];
+        return dp[W]; // Max value with given weight
     }
 }
-
