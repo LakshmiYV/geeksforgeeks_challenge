d23c7d77ec844e2b02a80230d27a948a1367e61e
LakshmiYV <161949763+LakshmiYV@users.noreply.github.com>
2025-03-30 14:27:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Gas Station/gas-station.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+
+            ArrayList<Integer> array1 = new ArrayList<Integer>();
+            ArrayList<Integer> array2 = new ArrayList<Integer>();
+
+            String line = read.readLine();
+            String[] tokens = line.split(" ");
+            for (String token : tokens) {
+                array1.add(Integer.parseInt(token));
+            }
+            line = read.readLine();
+            tokens = line.split(" ");
+            for (String token : tokens) {
+                array2.add(Integer.parseInt(token));
+            }
+
+            // ArrayList<Integer> v = new ArrayList<Integer>();
+            int[] gas = new int[array1.size()];
+            int idx = 0;
+            for (int i : array1) gas[idx++] = i;
+
+            int[] cost = new int[array2.size()];
+            idx = 0;
+            for (int i : array2) cost[idx++] = i;
+
+            int ans = new Solution().startStation(gas, cost);
+
+            System.out.println(ans);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public int startStation(int[] gas, int[] cost) {
+        int totalGas = 0, totalCost = 0;
+        int startIndex = 0, tank = 0;
+        
+        for (int i = 0; i < gas.length; i++) {
+            totalGas += gas[i];
+            totalCost += cost[i];
+            tank += gas[i] - cost[i];
+            
+            if (tank < 0) { // If we can't reach the next station
+                startIndex = i + 1; // Start from the next station
+                tank = 0; // Reset the tank
+            }
+        }
+        
+        return totalGas >= totalCost ? startIndex : -1;
+    }
+}
+
